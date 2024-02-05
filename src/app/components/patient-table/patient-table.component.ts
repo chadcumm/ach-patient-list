11ed50d9a06ba04bab0ad959c0d4ac1f8aa07d7c
chad cummings <algeron@gmail.com>
2024-02-05 15:17:57 -0600

    initial implement of cell clicks, column and context

--- src/app/components/patient-table/patient-table.component.ts
+++ src/app/components/patient-table/patient-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PopulationDataService } from 'src/app/service/population-data.service';
-
+import { MatDialog } from '@angular/material/dialog';
+import { MPageConfirmComponent } from '@clinicaloffice/clinical-office-mpage';
 @Component({
   selector: 'app-patient-table',
   templateUrl: './patient-table.component.html',
@@ -9,10 +10,72 @@ import { PopulationDataService } from 'src/app/service/population-data.service';
 export class PatientTableComponent implements OnInit {
 
   constructor(
-    public patientListDS: PopulationDataService
+    public patientListDS: PopulationDataService,
+    public dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
+
   }
 
+  public cellColumnClick(event: any): void {
+    console.log('Cell Column Click:', event);
+
+    if (event.column === 'patientName') {
+      console.log('Patient Name Clicked:', event.row);
+      this.CernerApplicationAction('OPENCHART', event.row)
+    } else {
+
+      const dialogRef = this.dialog.open(MPageConfirmComponent, {
+        width: '500px',
+        data: {
+            title: 'Cell Column Click',
+            text: JSON.stringify(event),
+            icon: 'info'
+        }
+      });
+    }
+  }
+ 
+
+  public cellContextMenu(event: any): void {
+    console.log('Cell Row Click:', event);
+    if (event.contextMenuAction) {
+      const dialogRef = this.dialog.open(MPageConfirmComponent, {
+        width: '500px',
+        data: {
+            title: 'Row Column Click',
+            text: JSON.stringify(event),
+            icon: 'info'
+        }
+      });
+    }
+  }
+
+  CernerApplicationAction(action: string, data: any): void {
+    console.log('action: ' + action + 'data.encntrId: ' + data.encntrId + ' data.personId: ' + data.personId)
+    if (data.encntrId && data.personId) {
+      const el = document.getElementById('applink');
+
+      switch (action) {
+        case 'OPENCHART':
+          if (data.personId !== undefined && data.encntrId !== undefined) {
+            // @ts-ignore
+            el.href = 'javascript:APPLINK(0,"Powerchart.exe","/PERSONID=' + data.personId + ' /ENCNTRID=' + data.encntrId + '")';
+
+          } else if (data.personId !== undefined) {
+            // @ts-ignore
+            el.href = 'javascript:APPLINK(0,"Powerchart.exe","/PERSONID=' + data.personId + '")';
+          }
+
+          // @ts-ignore
+          el.click();
+
+          break;
+        default:
+          alert('The action ' + action + ' has not been defined.');
+          break;
+      }
+    }
+  }
 }
